a35ecb785d85d3958a2b6b2102d9f3eef91b7994
clagosuarez <55792889+clagosuarez@users.noreply.github.com>
2019-10-15 10:15:06 +0200

    Add files via upload

---
+++ bol4_2/Bol4_2.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bol4_2;
+
+/**
+ *
+ * @author clagosuarez
+ */
+public class Bol4_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Satelite luna = new Satelite(20, 12,384400);
+        luna.verPosicion();
+    }
+    
+}
